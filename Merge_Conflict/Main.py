6e75cc375637b9fc363f4af3abbcc96f1e6125f1
Vidura_Bandara <vidurabandara125@gmail.com>
2026-01-08 02:02:27 +0530

    Main Branch

---
+++ Merge_Conflict/Main.py
@@ -0,0 +1,13 @@
+class Person:
+    def __init__(self, name=None, age=None):
+        self._name = name
+        self._age = age
+    
+    def greet(self):
+        print(f"Hello, my name is {self._name} and I am {self._age} years old.") 
+
+
+
+
+man1 = Person("Alice", 30)
+man1.greet()
